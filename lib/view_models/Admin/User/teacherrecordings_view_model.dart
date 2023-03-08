1a772b2ee845b616ff70100702ec904ab8f2560c
JuanGar123 <JuanGar123>
2023-03-08 07:44:46 +0500

    done

--- lib/view_models/Admin/User/teacherrecordings_view_model.dart
+++ lib/view_models/Admin/User/teacherrecordings_view_model.dart
@@ -1,76 +1,103 @@
 // ignore_for_file: prefer_final_fields, prefer_typing_uninitialized_variables
 
 import 'package:flutter/cupertino.dart';
-import 'package:live_streaming/Model/Admin/teacherrecordings.dart';
+import 'package:live_streaming/Model/Admin/recordings.dart';
 import 'package:live_streaming/Model/user_error.dart';
 import 'package:live_streaming/repo/Admin/teacher_recording_services.dart';
 import 'package:live_streaming/repo/api_status.dart';
+import 'package:video_player/video_player.dart';
 
 class TeacherRecordingsViewModel with ChangeNotifier {
   bool _isloading = false;
   var _teacherrecordings;
   UserError? _userError;
   String filter = "Section";
+  List<Recordings> tempTeacherRecordings = [];
+  var _videoController;
+  Future<void>? _initializeVideoPlayerFuture;
+  var _selectedVideo;
+  bool isshow = false;
 
   bool get loading => _isloading;
   UserError? get userError => _userError;
-  TeacherRecordings get teacherrecordings => _teacherrecordings;
-  TeacherRecordings? tempteacherrecordings = TeacherRecordings();
-  TeacherRecordingsViewModel(int teacherid) {
-    getData(teacherid);
+  List<Recordings> get teacherrecordings => _teacherrecordings;
+  VideoPlayerController get videoController => _videoController;
+
+  Recordings get selectedVideo => _selectedVideo;
+
+  TeacherRecordingsViewModel(String teacherName) {
+    getData(teacherName);
   }
   setloading(bool loading) async {
     _isloading = loading;
     notifyListeners();
   }
 
+  setSelectedVideo(Recordings recordings) {
+    _selectedVideo = recordings;
+
+    notifyListeners();
+  }
+
+  // getVideoPosition() {
+  //   var duration = Duration(
+  //       milliseconds: videoController.value.position.inMilliseconds.round());
+  //   time = [duration.inMinutes, duration.inSeconds]
+  //       .map((seg) => seg.remainder(60).toString().padLeft(2, '0'))
+  //       .join(':');
+  // }
+
+  setPlayer(String path) async {
+    setloading(true);
+    _videoController = VideoPlayerController.network(path);
+    _initializeVideoPlayerFuture = videoController.initialize();
+    _initializeVideoPlayerFuture?.then((value) => setloading(false));
+  }
+
+  setIsShow() {
+    isshow = !isshow;
+    notifyListeners();
+  }
+
   void setFilter(String value) {
     if (value != "") {
       if (filter == "Date") {
-        tempteacherrecordings!.recordings = teacherrecordings.recordings!
+        tempTeacherRecordings = teacherrecordings
             .where((element) => element.date.toString().contains(value))
             .toList();
       } else if (filter == "Type") {
-        tempteacherrecordings!.recordings = teacherrecordings.recordings!
-            .where((element) => element.filename.toString().contains(value))
+        tempTeacherRecordings = teacherrecordings
+            .where((element) => element.fileName.toString().contains(value))
             .toList();
       } else if (filter == "Section") {
-        tempteacherrecordings!.section = teacherrecordings.section!
-            .where((element) => element.name.toString().contains(value))
+        tempTeacherRecordings = teacherrecordings
+            .where((element) => element.discipline.toString().contains(value))
             .toList();
       } else if (filter == "Course") {
-        tempteacherrecordings!.course = teacherrecordings.course!
-            .where((element) => element.name.toString().contains(value))
+        tempTeacherRecordings = teacherrecordings
+            .where((element) => element.courseName.toString().contains(value))
             .toList();
       }
     } else {
-      tempteacherrecordings!.recordings = teacherrecordings.recordings;
-      tempteacherrecordings!.course = teacherrecordings.course;
-      tempteacherrecordings!.section = teacherrecordings.section;
-      tempteacherrecordings!.teacherslot = teacherrecordings.teacherslot;
-      tempteacherrecordings!.timetable = teacherrecordings.timetable;
+      tempTeacherRecordings = teacherrecordings;
     }
     notifyListeners();
   }
 
-  void setTeacherRecordings(TeacherRecordings teacherrecordings) {
-    _teacherrecordings = teacherrecordings;
-    tempteacherrecordings!.recordings = teacherrecordings.recordings;
-    tempteacherrecordings!.course = teacherrecordings.course;
-    tempteacherrecordings!.section = teacherrecordings.section;
-    tempteacherrecordings!.teacherslot = teacherrecordings.teacherslot;
-    tempteacherrecordings!.timetable = teacherrecordings.timetable;
+  void setTeacherRecordings(List<Recordings> lst) {
+    tempTeacherRecordings = lst;
+    _teacherrecordings = lst;
   }
 
   void setUserError(UserError userError) {
     _userError = userError;
   }
 
-  getData(int teacherid) async {
+  getData(String teacherName) async {
     setloading(true);
-    var response = await TeacherRecordingServies.getRecordings(teacherid);
+    var response = await TeacherRecordingServies.getRecordings(teacherName);
     if (response is Success) {
-      setTeacherRecordings(response.response as TeacherRecordings);
+      setTeacherRecordings(response.response as List<Recordings>);
     }
     if (response is Failure) {
       UserError userError =
